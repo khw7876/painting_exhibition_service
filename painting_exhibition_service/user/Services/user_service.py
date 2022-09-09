be341135432a57c371a00eff0ea6bd1752d58798
khw7876 <khw7876@gmail.com>
2022-09-09 19:37:57 +0900

    feat/ request_user를 통한 로그인유저 식별 기능 추가

--- painting_exhibition_service/user/Services/user_service.py
+++ painting_exhibition_service/user/Services/user_service.py
@@ -14,16 +14,14 @@ def create_user(create_data : dict[str,str]) -> None:
     user_data_serializer.is_valid(raise_exception=True)
     user_data_serializer.save()
     
-def update_user(update_data : dict[str, str], user_id : int) -> None:
-    user_for_update = UserModel.objects.get(id=user_id)
-    update_data["cur_password"] = user_for_update.password
-    user_data_serializer = UserUpdateSerializer(user_for_update, data=update_data, partial=True)
+def update_user(update_data : dict[str, str], user: UserModel) -> None:
+    update_data["cur_password"] = user.password
+    user_data_serializer = UserUpdateSerializer(user, data=update_data, partial=True)
     user_data_serializer.is_valid(raise_exception=True)
     user_data_serializer.save()
 
-def check_password_is_possible(password, user_id):
-    user_for_check = UserModel.objects.get(id=user_id)
-    if (check_password(password, user_for_check.password)):
+def check_password_is_possible(password, user):
+    if (check_password(password, user.password)):
         return True
     return False
 
--- painting_exhibition_service/user/views.py
+++ painting_exhibition_service/user/views.py
@@ -19,9 +19,10 @@ class UserView(APIView):
         create_user(request.data)
         return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_200_OK)
 
-    def put(self, request : Request, user_id : int) -> Response:
-        if check_password_is_possible(request.data["password"], user_id):
-            update_user(request.data, user_id)
+    def put(self, request : Request) -> Response:
+        user = request.user
+        if check_password_is_possible(request.data["password"], user):
+            update_user(request.data, user)
             return Response({"detail" : "비밀번호가 변경되었습니다."}, status=status.HTTP_200_OK)
         return Response({"detail" : "기존의 비밀번호와 일치하는 비밀번호입니다."}, status=status.HTTP_400_BAD_REQUEST)
 
