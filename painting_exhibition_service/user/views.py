b5898b3de611f19783832cc23ce8019ea3b63a5b
khw7876 <khw7876@gmail.com>
2022-09-09 18:32:59 +0900

    feat/ 회원가입 Service Layer 분리

--- painting_exhibition_service/user/views.py
+++ painting_exhibition_service/user/views.py
@@ -3,7 +3,9 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import UserSignupSerializer
+from user.Services.user_service import (
+    create_user
+)
 
 # Create your views here.
 class UserView(APIView):
@@ -11,7 +13,5 @@ class UserView(APIView):
     회원정보 조회 및 회원가입
     """
     def post(self, request: Request) -> Response:
-        user_data_serializer = UserSignupSerializer(data=request.data)
-        user_data_serializer.is_valid(raise_exception=True)
-        user_data_serializer.save()
+        create_user(request.data)
         return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_200_OK)
