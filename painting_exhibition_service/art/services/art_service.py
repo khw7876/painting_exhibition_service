273e1b8370b140089618b8c4a675b80626ea967f
khw7876 <khw7876@gmail.com>
2022-09-12 06:20:02 +0900

    feat/ 작가의 작품등록 구현

--- painting_exhibition_service/art/services/art_service.py
+++ painting_exhibition_service/art/services/art_service.py
@@ -1,6 +1,16 @@
 from user.models import User as UserModel
-
+from art.serializers import ArtSerializer
+from applicant.models import Artist as ArtistModel
 def check_is_artist(user : UserModel):
-    if user.artist_set:
+    try:
+        ArtistModel.objects.get(user=user.id)
         return True
-    return False
+    except ArtistModel.DoesNotExist:
+        return False
+
+def create_art(create_data, user):
+    create_data["artist"] = ArtistModel.objects.get(user=user.id).id
+    art_serializer = ArtSerializer(data=create_data)
+    art_serializer.is_valid(raise_exception=True)
+    art_serializer.save()
+
--- painting_exhibition_service/art/views.py
+++ painting_exhibition_service/art/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 # Create your views here.
 from .services.art_service import (
     check_is_artist,
+    create_art,
 )
 
 
@@ -11,5 +12,6 @@ class ArtView(APIView):
 
     def post(self, request):
         if check_is_artist(request.user):
-            return Response()
+            create_art(request.data, request.user)
+            return Response({"detail" : "작품을 등록하였습니다."}, status=status.HTTP_200_OK)
         return Response({"detail" : "관리자만이 접근이 가능합니다."}, status=status.HTTP_400_BAD_REQUEST)
